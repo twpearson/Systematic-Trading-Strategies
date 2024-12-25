1cf433e643a920e32c6d243b7dfc8113e2b40272
twpearson <tom.wp6@gmail.com>
2024-12-25 09:30:22 +0000

    begin tidying script

--- Week1/Systematic Trading Strategies-Week1.R
+++ Week1/Systematic Trading Strategies-Week1.R
@@ -1,6 +1,7 @@
           # ******************************QUESTION 1******************************
 
 # **************SET WORKING DIRECTORY AND CLEAR ENVIRONMENT ************************************************************
+library(conflicted)
 library(rstudioapi)  # This is a external library of functions
 library(dplyr)
 # Getting the path of your current open file
@@ -17,42 +18,43 @@ x <- length(unique(stock$date))
 
 #remove stocks which trade less than trading dates
 stock_data <- stock_data %>%
-  group_by(symbol) %>%
-  filter(n() == x)
+  dplyr::group_by(symbol) %>%
+  dplyr::filter(n() == x)
 
 #Getting all openings on 2019-01-03(first trading day)
 stock_data_start <- stock_data %>%
-  group_by(symbol)%>%
-  filter(date == "2019-01-03" )
+  dplyr::group_by(symbol) %>%
+  dplyr::filter(date == "2019-01-03" )
 
 #Getting all closings on 2019-12-30(last trading day)
 stock_data_end <- stock_data %>%
-  group_by(symbol)%>%
-  filter(date == "2019-12-30" )
+  dplyr::group_by(symbol) %>%
+  dplyr::filter(date == "2019-12-30" )
 
 #Calculating Annual returns
-annual_return <- ((stock_data_end$close-stock_data_start$open)/stock_data_start$open)*100
-format(round(annual_return,1))
+annual_return <- ((stock_data_end$close - stock_data_start$open) / stock_data_start$open) * 100
+format(round(annual_return, digits = 1))
 
 #Attaching annual returns to the data
-stock_data_end <- cbind(stock_data_end,annual_return = annual_return)
+stock_data_end <- cbind(stock_data_end, annual_return = annual_return)
 
 #subsetting data to get stock symbols and annual returns
-stock_data_end <- stock_data_end[c(1,8)]
+stock_data_end <- stock_data_end %>%
+  dplyr::select(symbol, annual_return)
 
 # Identify the top 10 stocks in terms of total annual return
 top_10_stocks <- stock_data_end %>%
-  top_n(10,annual_return ) %>%
-  arrange(desc(annual_return ))
+  dplyr::top_n(10, annual_return) %>%
+  dplyr::arrange(desc(annual_return ))
 
 # Identify the bottom 10 stocks in terms of total annual return
 bottom_10_stocks <- stock_data_end %>%
-  top_n(-10, annual_return ) %>%
-  arrange(annual_return )
+  dplyr::top_n(-10, annual_return) %>%
+  dplyr::arrange(annual_return)
 
-top_10_stocks$annual_return <- format(round(top_10_stocks$annual_return,1))
+top_10_stocks$annual_return <- format(round(top_10_stocks$annual_return, digits = 1))
 
-bottom_10_stocks$annual_return <- format(round(bottom_10_stocks$annual_return,1))
+bottom_10_stocks$annual_return <- format(round(bottom_10_stocks$annual_return, digits = 1))
 # Print the top 10 and bottom 10 stocks
 print("Top 10 stocks:")
 print(top_10_stocks)
@@ -81,37 +83,38 @@ stock_data <-stock
 x <- length(unique(stock$date))
 #remove stocks which trade less than trading dates
 stock_data <- stock %>%
-  group_by(symbol) %>%
-  filter(n() == x)
+  dplyr::group_by(symbol) %>%
+  dplyr::filter(n() == x)
 
 #Getting all openings on 2019-01-03
 stock_data_start <- stock_data %>%
-  group_by(symbol)%>%
-  filter(date == "2019-01-03" )
+  dplyr::group_by(symbol)%>%
+  dplyr::filter(date == "2019-01-03" )
 
 #Getting all closings on 2019-12-30
 stock_data_end <- stock_data %>%
-  group_by(symbol)%>%
-  filter(date == "2019-12-30" )
+  dplyr::group_by(symbol)%>%
+  dplyr::filter(date == "2019-12-30" )
 
 
 #Calculating Annual returns
-annual_return <- ((stock_data_end$close-stock_data_start$open)/stock_data_start$open)*100
-format(round(annual_return,1))
+annual_return <- ((stock_data_end$close - stock_data_start$open) / stock_data_start$open) * 100
+format(round(annual_return, digits = 1))
 
 #Attaching annual returns to the data
-stock_data_end <- cbind(stock_data_end,annual_return = annual_return)
-stock_data_end <- stock_data_end[c(1,8)]
-sectors <- merge(sectors,stock_data_end )
+stock_data_end <- cbind(stock_data_end, annual_return = annual_return)
+stock_data_end <- stock_data_end %>%
+  dplyr::select(symbol, annual_return)
+sectors <- dplyr::left_join(sectors, stock_data_end, by = "symbol")
 
 
-stock_data_end <- merge(stock_data_end,sectors)
+stock_data_end <- dplyr::left_join(stock_data_end, sectors, by = c("symbol", "annual_return"))
 
 
 
 mean_return_df <- sectors %>%
-  group_by(sector) %>%
-  summarize(mean_return = mean(annual_return))
+  dplyr::group_by(sector) %>%
+  dplyr::summarize(mean_return = mean(annual_return))
 
 mean_return_df[order(mean_return_df$mean_return, decreasing = TRUE),]
 
@@ -139,24 +142,22 @@ x <- length(unique(stock$date))
 
 #remove stocks which trade less than total trading dates
 stock_data <- stock %>%
-  group_by(symbol) %>%
-  filter(n() == x)
+  dplyr::group_by(symbol) %>%
+  dplyr::filter(n() == x)
 
 #grouping stocks by month
 stock_data <- stock_data %>%
-  group_by(month = lubridate::floor_date(date, 'month'))
+  dplyr::group_by(month = lubridate::floor_date(date, 'month'))
 
 #Extracting month from dates
-stock_data$month <- format(stock_data$date,"%m")
+stock_data$month <- format(stock_data$date, "%m")
 
 
 #calculating monthly return for each stock
-mat= NULL
+mat = NULL
 x <- c("01","02","03","04","05","06","07","08","09","10","11","12")
-for (symbol in sectors$symbol)
-{
-  for(val in x)
-  {
+for (symbol in sectors$symbol) {
+  for(val in x) {
     #print(x)
     c<-tail(stock_data[stock_data$symbol==symbol & stock_data$month==val,],1)
     o<-head(stock_data[stock_data$symbol==symbol & stock_data$month==val,],1)
@@ -183,10 +184,8 @@ mat1 = NULL
 rows <- unique(sectors$sector)
 
 
-for(r in rows)
-{
-  for(val in x)
-  {
+for(r in rows) {
+  for(val in x) {
     
     mat1 <- rbind(mat1,c(r,val,mean(as.numeric(mat$monthly_return[mat$sector==r & mat$month==val]))))
     #  ans[r][val] <- mean(as.numeric(mat$monthly_return[mat$sector==r & mat$month==val]))    
@@ -203,8 +202,7 @@ ans = matrix(, nrow = length(unique(sectors$sector)), ncol = 12)
 rnames=NULL
 ans = NULL
 ans1 =NULL
-for(sector in rows)
-{
+for(sector in rows) {
   rnames <- rbind(rnames,sector)
   ans1 <- rbind(mat1$monthly_return[mat1$sector == sector])
   ans <- rbind(ans,ans1)
@@ -243,14 +241,13 @@ stock_data <-stock[stock$symbol=="AAPL",]
 
 # Getting Daily Returns for apple stock
 stock_data<-stock_data %>%
-  mutate(daily_returns = ((close-open)/open))
+  dplyr::mutate(daily_returns = ((close-open)/open))
 
 #Calculating Cummulative and Max Cummulative Returns
 stock_data$daily_returns = 1+stock_data$daily_returns
 cumm_ret = stock_data$daily_returns[1]
 max_cumm_return = cumm_ret
-for(i in 2:250)
-{
+for(i in 2:250) {
   cumm_ret<- rbind(cumm_ret,cumm_ret[i-1]*(stock_data$daily_returns[i]))
   max_cumm_return<- rbind(max_cumm_return,max(max_cumm_return,cumm_ret))
 }
@@ -260,20 +257,34 @@ colnames(cumm_ret) <- c("cumm_return")
 stock_data <- cbind(stock_data,cumm_ret,max_cumm_return)
 
 #Printing the plots for the returns
-d_r_p<-ggplot(stock_data, aes(stock_data$date, stock_data$daily_returns))+ geom_line(size = 1, color = 'red') + labs(y="daily returns in %", x="Date")+ggtitle("Daily Returns")
+d_r_p <-
+  ggplot(stock_data, aes(date, daily_returns)) + 
+  geom_line(size = 1, color = 'red') + 
+  labs(y="daily returns in %", x="Date")+
+  ggtitle("Daily Returns")
 print( d_r_p )
 
-c_r_p<-ggplot(stock_data, aes(stock_data$date, stock_data$cumm_ret))+ geom_line(size = 1, color = 'green') + labs(y="cumm returns in %", x="Date")+ggtitle("Cummulative Returns")
+c_r_p <-
+  ggplot(stock_data, aes(date, cumm_ret)) + 
+  geom_line(size = 1, color = 'green') + 
+  labs(y="cumm returns in %", x="Date") +
+  ggtitle("Cummulative Returns")
 print( c_r_p )
 
-m_r_p<-ggplot(stock_data, aes(stock_data$date, stock_data$max_cumm_return))+ geom_line(size = 1, color = 'blue') +labs(y="max cumm returns in %", x="Date")+ggtitle("Max Cummulative Returns")
+m_r_p <-
+  ggplot(stock_data, aes(date, max_cumm_return)) +
+  geom_line(size = 1, color = 'blue') +
+  labs(y="max cumm returns in %", x="Date") +
+  ggtitle("Max Cummulative Returns")
 print( m_r_p)
 
-p4 <- ggplot(stock_data, aes(x = date)) +
-  geom_line(aes(y = stock_data$cumm_ret, group = 1, color = "Cumulative Return")) +
-  geom_line(aes(y = stock_data$daily_returns, group = 1, color = "Daily Return"), linewidth = 0.7) +
-  geom_line(aes(y = stock_data$max_cumm_return, group = 1, color = "Max Cumulative Return"), linetype = "dashed")+
-  labs(y="returns in %", x="Date") + ggtitle("Combined Plot")
+p4 <- 
+  ggplot(stock_data, aes(x = date)) +
+  geom_line(aes(y = cumm_ret, group = 1, color = "Cumulative Return")) +
+  geom_line(aes(y = daily_returns, group = 1, color = "Daily Return"), linewidth = 0.7) +
+  geom_line(aes(y = max_cumm_return, group = 1, color = "Max Cumulative Return"), linetype = "dashed")+
+  labs(y="returns in %", x="Date") + 
+  ggtitle("Combined Plot")
 
 print(p4)
 grid.arrange(d_r_p, c_r_p,m_r_p,p4, nrow = 2)
